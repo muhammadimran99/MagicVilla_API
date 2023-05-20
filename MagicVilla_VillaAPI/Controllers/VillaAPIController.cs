268c11d4d012ce4a4cf78ecd47883eb53e43db8e
imran <imrankhan99281@gmail.com>
2023-05-20 19:10:36 +0500

    Create a HttpGEet Method for API

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1 , Name = "Pool View"},
+                new Villa { Id = 2, Name = "Beach View"}
+            };
+        }
+    }
+}
